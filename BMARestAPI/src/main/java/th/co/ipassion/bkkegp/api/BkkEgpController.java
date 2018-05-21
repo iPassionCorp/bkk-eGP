ce80b9408e9a72229d5ef8a77a6d0b23d5f905c5
lhll2o <chatchai.s@ipassion.co.th>
2018-05-21 16:12:22 +0700

    no message

--- BMARestAPI/src/main/java/th/co/ipassion/bkkegp/api/BkkEgpController.java
+++ BMARestAPI/src/main/java/th/co/ipassion/bkkegp/api/BkkEgpController.java
@@ -7,23 +7,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import th.co.ipassion.bkkegp.BMAConstant;
 import th.co.ipassion.bkkegp.dao.DAOException;
 import th.co.ipassion.bkkegp.dao.RssEgpDao;
 import th.co.ipassion.bkkegp.model.RssEgp;
 
 @RestController
-public class BkkEgpController {  
+public class BkkEgpController {
 	@RequestMapping("/listBkkEgp")
     @CrossOrigin(origins = "*", maxAge = 3600)
-    public List<RssEgp> listBkkEgp(@RequestParam(value="deptId", defaultValue="3100001") String deptId) {
+    public List<RssEgp> listBkkEgp(
+    		@RequestParam(value="deptId", defaultValue=BMAConstant.DEPTID_BKK) String deptId, 
+    		@RequestParam(value="announceType", defaultValue="*") String announceType,
+    		@RequestParam(value="methodId", defaultValue="*") String methodId
+    		) {
     	RssEgpDao service = new RssEgpDao();
     	List<RssEgp> result = null;
     	try { 
-    		if (deptId.equalsIgnoreCase("3100001") || deptId.equalsIgnoreCase("*")) {
+    		if (deptId.equalsIgnoreCase(BMAConstant.DEPTID_BKK) 
+    				&& "*".equalsIgnoreCase(announceType)
+    				&& "*".equalsIgnoreCase(methodId)) {
+    			
     			result = service.getAllEgpInfo();
-    		} else {
+    			
+    		} else if (!deptId.equalsIgnoreCase(BMAConstant.DEPTID_BKK) 
+    				&& "*".equalsIgnoreCase(announceType)
+    				&& "*".equalsIgnoreCase(methodId)) {
+    			
     			result = service.getEgpInfoBySubDeptCodeId(deptId);
+    			
+    		} else {
+    			
+    			if ("*".equalsIgnoreCase(announceType)) {	
+    				announceType = BMAConstant.DEFAULT_ANNOUNCE_TYPE;
+    			}
+    			
+    			result = service.getEgpInfoByCondition(deptId, announceType, methodId);
+    			
     		}
+    		
+    		
     	} catch (DAOException e) { e.printStackTrace(); }
     	return result;
-    }}
+    }
+}
