6f9df244de76db3af62ad545e08755e4e58832f7
lhll2o <chatchai.s@ipassion.co.th>
2018-05-21 14:36:48 +0700

    update controller @RequestParam

--- BMARestAPI/src/main/java/th/co/ipassion/bkkegp/api/BkkEgpController.java
+++ BMARestAPI/src/main/java/th/co/ipassion/bkkegp/api/BkkEgpController.java
@@ -3,6 +3,7 @@ package th.co.ipassion.bkkegp.api;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import th.co.ipassion.bkkegp.dao.AnnounceTypeDao;
 import th.co.ipassion.bkkegp.dao.DAOException;
 import th.co.ipassion.bkkegp.dao.MethodIDTypeDao;
 import th.co.ipassion.bkkegp.dao.RssEgpDao;
+import th.co.ipassion.bkkegp.model.DataJson;
 import th.co.ipassion.bkkegp.model.MapValue;
 import th.co.ipassion.bkkegp.model.RssEgp;
 
@@ -35,7 +37,7 @@ public class BkkEgpController {
 
     @RequestMapping("/listAnnounce")
     @CrossOrigin(origins = "*", maxAge = 3600)
-    public List<MapValue> getListAnnounce() {
+    public List<MapValue> getListAnnounce(@RequestParam(value="deptId", defaultValue="3100001") String deptId) {
     	AnnounceTypeDao service = new AnnounceTypeDao();
     	List<MapValue> result = null;
     	try { 
@@ -48,7 +50,7 @@ public class BkkEgpController {
     
     @RequestMapping("/listMethodId")
     @CrossOrigin(origins = "*", maxAge = 3600)
-    public List<MapValue> getListMethodId() {
+    public List<MapValue> getListMethodId(@RequestParam(value="deptId", defaultValue="3100001") String deptId) {
     	MethodIDTypeDao service = new MethodIDTypeDao();
     	List<MapValue> result = null;
     	try { 
@@ -57,5 +59,14 @@ public class BkkEgpController {
     		e.printStackTrace(); 
     	}
     	return result;
+    }
+    
+    @RequestMapping("/testJson")
+    @CrossOrigin(origins = "*", maxAge = 3600)
+    public DataJson testJson(@RequestBody DataJson j) {
+    	DataJson output = new DataJson();
+    	output.setKey("key Json : " + j.getKey());
+    	output.setValue("value_json : "  + j.getValue());
+    	return output;
     }
 }
